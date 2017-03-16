b30e153717c81c531ed205789f64697d97925352
BSKYB\RobertG <gareth.roberts@sky.uk>
2017-03-16 15:16:14 +0000

    completed wordcount exercise

--- basic/wordcount.py
+++ basic/wordcount.py
@@ -44,8 +44,29 @@ import sys
 # You could write a helper utility function that reads a file
 # and builds and returns a word/count dict for it.
 # Then print_words() and print_top() can just call the utility function.
+def print_words(filename):
+    dict = helper(filename)
+    print sorted(dict.items())
 
-###
+def print_top(filename):
+    dict = helper(filename)
+    list = sorted(dict, key=lambda i: int(dict[i]),reverse=True)
+    for key in list[:20]:
+        print key, dict[key]
+
+def helper(filename):
+    dict = {}
+    f = open(filename,'rU')
+    for line in f:
+        s = line.split()
+        for word in s:
+            lowercase = word.lower()
+            if lowercase in dict:
+                dict[lowercase] = dict[lowercase] + 1
+            else:
+                dict[lowercase] = 1
+    f.close()
+    return dict
 
 # This basic command line argument parsing code is provided and
 # calls the print_words() and print_top() functions which you must define.
